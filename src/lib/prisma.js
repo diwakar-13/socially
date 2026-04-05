5716695f231dda379180f6d54a61350ccd9f1bce
Diwakar pandey <diwakarpandey410@gmail.com>
2026-04-05 21:33:46 +0530

    refactor: standardize import statements and formatting in prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,6 @@
-import 'dotenv/config'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@/generated/prisma/client'
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
 
 // Global object ko access karne ke liye (Next.js 15/16 safe way)
 const globalForPrisma = globalThis;
@@ -8,15 +8,15 @@ const globalForPrisma = globalThis;
 const prismaClientSingleton = () => {
   const adapter = new PrismaNeon({
     connectionString: process.env.DATABASE_URL,
-  })
-  return new PrismaClient({ adapter })
-}
+  });
+  return new PrismaClient({ adapter });
+};
 
-// Agar global memory mein pehle se prisma hai toh wahi use karo, 
+// Agar global memory mein pehle se prisma hai toh wahi use karo,
 // nahi toh naya banao
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 // Dev mode mein prisma ko global object mein save kar lo
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
